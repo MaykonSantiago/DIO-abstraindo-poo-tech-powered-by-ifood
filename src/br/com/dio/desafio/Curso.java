60ad72406131558f165045c126532002db1d3483
Maykon Santiago <maykonsantiago.ms@gmail.com>
2022-11-05 20:37:21 -0300

    Herança e polimorfismo, parte 1.

--- src/br/com/dio/desafio/Curso.java
+++ src/br/com/dio/desafio/Curso.java
@@ -1,26 +1,17 @@
 package br.com.dio.desafio;
 
-public class Curso {
-    private String titulo;
+public class Curso extends Conteudo{
     private int cargaHoraria;
-    private String descricao;
 
     public Curso() {
 
     }
 
-    public String getTitulo() {
-        return titulo;
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO * cargaHoraria;
     }
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-    public String getDescricao() {
-        return descricao;
-    }
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
+
     public int getCargaHoraria() {
         return cargaHoraria;
     }
@@ -31,8 +22,8 @@ public class Curso {
     @Override
     public String toString() {
         return "Curso {" + '\n' +
-                "    Título= " + this.titulo + ",\n" +
-                "    Descrição= " + this.descricao + ",\n" +
+                "    Título= " + getTitulo() + ",\n" +
+                "    Descrição= " + getDescricao() + ",\n" +
                 "    Carga horaria= " + this.cargaHoraria + ",\n" +
                 '}';
     }
